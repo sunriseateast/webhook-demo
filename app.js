4a8c79f2cb6219eb965c443bf2959944cd265ae9
roshan <roshankadam5228@gmail.com>
2025-08-15 20:47:55 +0530

    add again response

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.post('/existing-number', (req, res) => {
   const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
   console.log(`\n\nWebhook received ${timestamp}\n`);
   console.log(JSON.stringify(req.body, null, 2));
-  res.status(200).end();
+//   res.status(200).end();
   res.send('ok')
 });
 
